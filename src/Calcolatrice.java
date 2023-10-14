7faafff2a46f5cd2978d3aa3f1ce5528e563aa00
Claudio <peronclaudio1@gmail.com>
2023-10-14 17:17:02 +0200

    metodo per tradurre da notazione infissa a postfissa FUNZIONANTE

--- src/Calcolatrice.java
+++ src/Calcolatrice.java
@@ -5,22 +5,97 @@ public class Calcolatrice {
     private JPanel Panel;
     private JPanel displayContainer;
     private JLabel display;
-    private JButton a7Button;
-    private JButton a8Button;
-    private JButton a9Button;
-    private JButton button4;
-    private JButton a4Button;
-    private JButton a5Button;
-    private JButton a6Button;
-    private JButton button8;
-    private JButton button9;
-    private JButton a3Button;
-    private JButton a2Button;
-    private JButton a1Button;
+    private JButton btnSette;
+    private JButton btnOtto;
+    private JButton btnNove;
+    private JButton btnDiviso;
+    private JButton btnQuattro;
+    private JButton btnCinque;
+    private JButton btnSei;
+    private JButton btnPer;
+    private JButton btnMeno;
+    private JButton btnTre;
+    private JButton btnDue;
+    private JButton btnUno;
     private JPanel buttonContainer;
+    private JButton btnPiu;
+    private JButton btnUguale;
+    private JButton btnPunto;
+    private JButton btnZero;
+    private JButton btnAperta;
+    private JButton btnChiusa;
+    private JCheckBox checkBox1;
+    private JButton btnC;
+    private JButton btnAc;
     private CalcolatriceRPN calcolatrice;
     public Calcolatrice() {
         calcolatrice = new CalcolatriceRPN();
+
+        btnZero.addActionListener(e -> {
+            display.setText(display.getText() + "0");
+        });
+        btnUno.addActionListener(e -> {
+            display.setText(display.getText() + "1");
+        });
+        btnDue.addActionListener(e -> {
+            display.setText(display.getText() + "2");
+        });
+        btnTre.addActionListener(e -> {
+            display.setText(display.getText() + "3");
+        });
+        btnQuattro.addActionListener(e -> {
+            display.setText(display.getText() + "4");
+        });
+        btnCinque.addActionListener(e -> {
+            display.setText(display.getText() + "5");
+        });
+        btnSei.addActionListener(e -> {
+            display.setText(display.getText() + "6");
+        });
+        btnSette.addActionListener(e -> {
+            display.setText(display.getText() + "7");
+        });
+        btnOtto.addActionListener(e -> {
+            display.setText(display.getText() + "8");
+        });
+        btnNove.addActionListener(e -> {
+            display.setText(display.getText() + "9");
+        });
+        btnPunto.addActionListener(e -> {
+            display.setText(display.getText() + ".");
+        });
+        btnPiu.addActionListener(e -> {
+            display.setText(display.getText() + "+");
+        });
+        btnMeno.addActionListener(e -> {
+            display.setText(display.getText() + "-");
+        });
+        btnPer.addActionListener(e -> {
+            display.setText(display.getText() + "*");
+        });
+        btnDiviso.addActionListener(e -> {
+            display.setText(display.getText() + "/");
+        });
+        btnAperta.addActionListener(e -> {
+            display.setText(display.getText() + "(");
+        });
+        btnChiusa.addActionListener(e -> {
+            display.setText(display.getText() + ")");
+        });
+        btnAc.addActionListener(e -> {
+            display.setText("");
+        });
+        btnC.addActionListener(e -> {
+            String text = display.getText();
+            if (!text.isEmpty()) {
+                display.setText(text.substring(0, text.length() - 1));
+            }
+        });
+        btnUguale.addActionListener(e -> {
+            calcolatrice.setNotazioneInfissa(display.getText());
+            calcolatrice.trasformaInPostfissa();
+            display.setText(calcolatrice.getNotazionePostfissa());
+        });
     }
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calcolatrice");
--- src/CalcolatriceRPN.java
+++ src/CalcolatriceRPN.java
@@ -1,7 +1,93 @@
+import java.util.Stack;
+
 public class CalcolatriceRPN {
+    private String notazioneInfissa;
+    private String notazionePostfissa;
+    public CalcolatriceRPN() {}
 
-    public CalcolatriceRPN() {
-
+    public String getNotazioneInfissa() {
+        return notazioneInfissa;
     }
 
+    public String getNotazionePostfissa() {
+        return notazionePostfissa;
+    }
+
+    public void setNotazioneInfissa(String notazioneInfissa) {
+        this.notazioneInfissa = notazioneInfissa;
+    }
+
+    public void setNotazionePostfissa(String notazionePostfissa) {
+        this.notazionePostfissa = notazionePostfissa;
+    }
+
+    private boolean isNumber(char c) {
+        if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7'
+                || c == '8' || c == '9') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private boolean isOperator(char c) {
+        if (c == '+' || c == '-' || c == '*' || c == '/') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private int getPriority(char c) {
+        if (c == '+' || c == '-') {
+            return 1;
+        }
+        else if (c == '*' || c == '/') {
+            return 2;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    private String outputToString(Stack<Character> output) {
+        StringBuilder result = new StringBuilder();
+        while (!output.isEmpty()) {
+            Character element = output.pop();
+            result.insert(0, element);
+        }
+        return result.toString();
+    }
+
+    public void trasformaInPostfissa() {
+        Stack<Character> operatori = new Stack<>();
+        Stack<Character> risultato = new Stack<>();
+
+        for (int i = 0; i < notazioneInfissa.length(); i++) {
+            char c = notazioneInfissa.charAt(i);
+            if (isNumber(c)) {
+                risultato.push(c);
+            } else if (c == '(') {
+                operatori.push(c);
+            } else if (c == ')') {
+                while (operatori.peek() != '(') {
+                    risultato.push(operatori.pop());
+                }
+                operatori.pop();
+            } else if (isOperator(c)) {
+                while (!operatori.isEmpty() && getPriority(c) <= getPriority(operatori.peek())) {
+                    risultato.push(operatori.pop());
+                }
+                operatori.push(c);
+            }
+        }
+
+        while (!operatori.isEmpty()) {
+            risultato.push(operatori.pop());
+        }
+
+        notazionePostfissa = outputToString(risultato);
+    }
 }
